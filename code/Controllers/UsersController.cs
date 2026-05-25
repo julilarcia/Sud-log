7a5c9e8f1b0ff9c9943bd35d0b1b38e13ff72bb6
julia <jdorobis@student.agh.edu.pl>
2026-05-25 18:57:09 +0200

    naprawa

--- code/Controllers/UsersController.cs
+++ code/Controllers/UsersController.cs
@@ -1,13 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
 using Microsoft.EntityFrameworkCore;
 using GameTracker.Helpers;
 using GameTracker.Models;
 using GameTracker.ViewModels;
-=======
-using GameTracker.Models;
-using GameTracker.Helpers;
->>>>>>> d1e6f029f72950e120414f1a398a648eec697857
 
 namespace GameTracker.Controllers
 {
@@ -20,7 +15,6 @@ namespace GameTracker.Controllers
             _context = context;
         }
 
-<<<<<<< HEAD
         private bool IsAdmin()
         {
             return string.Equals(HttpContext.Session.GetString("UserRole"), "Admin", StringComparison.OrdinalIgnoreCase);
@@ -96,61 +90,3 @@ namespace GameTracker.Controllers
         }
     }
 }
-=======
-        // Metoda pomocnicza - sprawdza czy zalogowany jest Admin
-        private bool IsAdmin()
-        {
-            return HttpContext.Session.GetString("UserRole") == "Admin";
-        }
-
-        // GET: /Users/Index (Wyświetlanie listy użytkowników)
-        public IActionResult Index()
-        {
-            if (!IsAdmin()) return RedirectToAction("Index", "Home"); // Wyrzuca intruza
-
-            var users = _context.Users.ToList();
-            return View(users);
-        }
-
-        // GET: /Users/Create (Wyświetlanie formularza dodawania)
-        [HttpGet]
-        public IActionResult Create()
-        {
-            if (!IsAdmin()) return RedirectToAction("Index", "Home");
-
-            return View();
-        }
-
-        // POST: /Users/Create (Odbieranie danych z formularza)
-        [HttpPost]
-        public IActionResult Create(string login, string password, string role)
-        {
-            if (!IsAdmin()) return RedirectToAction("Index", "Home");
-
-            // Sprawdzamy czy login jest już zajęty
-            if (_context.Users.Any(u => u.Login == login))
-            {
-                ModelState.AddModelError("", "Użytkownik o takim loginie już istnieje!");
-                return View();
-            }
-
-            // Automatycznie generujemy unikalny ApiKey dla nowego gracza
-            string generatedApiKey = Guid.NewGuid().ToString("N");
-
-            // Tworzymy nowego użytkownika
-            var newUser = new User
-            {
-                Login = login,
-                PasswordHash = PasswordHelper.HashPassword(password), // Znowu używamy świetnego helpera Osoby B!
-                Role = string.IsNullOrEmpty(role) ? "Player" : role,
-                ApiKey = generatedApiKey
-            };
-
-            _context.Users.Add(newUser);
-            _context.SaveChanges();
-
-            return RedirectToAction("Index"); // Po dodaniu wracamy do listy
-        }
-    }
-}
->>>>>>> d1e6f029f72950e120414f1a398a648eec697857
